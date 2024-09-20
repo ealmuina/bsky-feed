4dd3e55d3d0cd62a3bd05cc010df888c8c4519ab
Eddy Almuiña <almuina.e@gmail.com>
2024-09-20 13:09:31 +0200

    Fix serialization issue

--- server/feeds/response.go
+++ server/feeds/response.go
@@ -1,8 +1,31 @@
 package feeds
 
-import "bsky/storage/models"
+import (
+	"bsky/storage/models"
+	"encoding/json"
+)
 
 type Response struct {
-	Cursor string        `json:"cursor"`
-	Posts  []models.Post `json:"feed"`
+	Cursor string
+	Posts  []models.Post
+}
+
+func (r Response) MarshalJSON() ([]byte, error) {
+	result := map[string]any{
+		"cursor": r.Cursor,
+	}
+
+	posts := make([]any, len(r.Posts))
+	for i, post := range r.Posts {
+		serializedPost := map[string]any{
+			"post": post.Uri,
+		}
+		if post.Reason != nil {
+			serializedPost["reason"] = post.Reason
+		}
+		posts[i] = serializedPost
+	}
+	result["feed"] = posts
+
+	return json.Marshal(result)
 }
--- storage/models/post.go
+++ storage/models/post.go
@@ -1,42 +1,16 @@
 package models
 
 import (
-	"encoding/json"
 	"time"
 )
 
 type Post struct {
-	// Serialized
-	Uri    string
-	Reason map[string]string
-
-	// Non serialized
+	Uri         string
+	Reason      map[string]string
 	Rank        float64
 	CreatedAt   time.Time
 	AuthorDid   string
 	ReplyParent string
 	ReplyRoot   string
 	Language    string
-}
-
-func (p *Post) MarshalJSON() ([]byte, error) {
-	result := map[string]any{
-		"post": p.Uri,
-	}
-	if p.Reason != nil {
-		result["reason"] = p.Reason
-	}
-	return json.Marshal(result)
-}
-
-func (p *Post) UnmarshalJSON(b []byte) error {
-	dict := map[string]any{}
-	if err := json.Unmarshal(b, &dict); err != nil {
-		return err
-	}
-
-	p.Uri = dict["post"].(string)
-	p.Reason = dict["reason"].(map[string]string)
-
-	return nil
 }
