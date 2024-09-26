0ae66f6e220ac3079a910d43c61c365e08f7ae1a
Eddy Almuiña <almuina.e@gmail.com>
2024-09-26 21:02:39 +0200

    Enable meters endpoint

--- server/server.go
+++ server/server.go
@@ -8,6 +8,7 @@ import (
 	"bsky/utils"
 	"errors"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"os"
 	"strconv"
@@ -35,7 +36,7 @@ func (s *Server) Run() {
 	mux.HandleFunc("/.well-known/did.json", s.getDidJson)
 	mux.HandleFunc("/xrpc/app.bsky.feed.describeFeedGenerator", s.getDescribeFeedGenerator)
 	mux.HandleFunc("/xrpc/app.bsky.feed.getFeedSkeleton", s.getFeedSkeleton)
-	//mux.Handle("/metrics", promhttp.Handler())
+	mux.Handle("/metrics", promhttp.Handler())
 
 	wrappedMux := middleware.NewServerMiddleware(mux)
 
