ae232949cebca2b840ad62647209b9363cae4c96
Eddy Almuiña <almuina.e@gmail.com>
2024-11-24 23:48:14 +0100

    Merge migrations

--- storage/db/migrations/20240906141716_CreateUser.up.sql
+++ storage/db/migrations/20240906141716_CreateUser.up.sql
@@ -1,14 +1,15 @@
 CREATE TABLE users
 (
-    id              SERIAL PRIMARY KEY,
-    did             TEXT                                NOT NULL,
-    handle          VARCHAR(255),
+    id                SERIAL PRIMARY KEY,
+    did               TEXT NOT NULL,
+    handle            VARCHAR(255),
 
-    followers_count INTEGER,
-    follows_count   INTEGER,
-    posts_count     INTEGER,
+    followers_count   INT,
+    follows_count     INT,
+    posts_count       INT,
 
-    last_update     TIMESTAMP
+    last_update       TIMESTAMP,
+    refresh_frequency INT  NOT NULL DEFAULT 30
 );
 
 CREATE INDEX IF NOT EXISTS "idx_users_did" ON "users" ("did");
--- storage/db/migrations/20241124120142_AddUserRefreshFrequency.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users
-    ADD COLUMN refresh_frequency INT NOT NULL DEFAULT 30;
